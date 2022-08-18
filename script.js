6fd7e133b12c22062b9a2b3debbd890eb4e982a7
Semih Beyzade Örscelik <semihb.orscelik0@gmail.com>
2022-08-18 10:12:58 +0200

    project completed

--- script.js
+++ script.js
@@ -2,3 +2,27 @@ let container = document.querySelector('.container');
 let button = document.querySelector('a');
 let displayHex = document.querySelector('p');
 
+const validCharacters = '0123456789ABCDEF';
+const hexDecimalLength = 6;
+
+function randomIntFromInterval(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getNextRandomColor(){
+   let color = '#';
+   for(let i = 0; i < 6; i++){
+       let getRandomChar = validCharacters.charAt(randomIntFromInterval(0,15))
+       color += getRandomChar;
+   };
+
+   displayHex.innerHTML = color;
+   container.style.backgroundColor = color;
+}
+
+getNextRandomColor();
+
+button.addEventListener('click', function(){
+  getNextRandomColor();
+
+},false)
